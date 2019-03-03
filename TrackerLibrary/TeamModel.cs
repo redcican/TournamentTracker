43cf8c3947245e8fcb2b512ed663f0d2906be5d1
redcican <cican17@gmail.com>
2019-03-03 17:03:48 +0100

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        /// <summary>
+        /// Represents a Teammbers class
+        /// </summary>
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
+        /// <summary>
+        /// Represent the Name of a Team
+        /// </summary>
+        public string TeamName { get; set; }
+    }
+}
---
+++ TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        public string TournamentName { get; set; }
+
+        public decimal EntryFee { get; set; }
+
+        public List<TeamModel> EnteredTeams { get; set; }
+
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+
+        public List<List<MatchUpModel>> Rounds { get; set; } = new List<List<MatchUpModel>>();
+
+    }
+}

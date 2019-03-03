ef0248a3b29506f3a3665ad792159021ea5cba34
redcican <cican17@gmail.com>
2019-03-03 17:12:49 +0100

    Add the comments to the PersonModel class

--- TrackerLibrary/PersonModel.cs
+++ TrackerLibrary/PersonModel.cs
@@ -8,12 +8,24 @@ namespace TrackerLibrary
 {
     public class PersonModel
     {
+        /// <summary>
+        /// Represents the first name of a person
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Represents the last name of a person
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Represents the email address of a person
+        /// </summary>
         public string EmailAddress { get; set; }
 
+        /// <summary>
+        /// Represents the cellphone number of a person
+        /// </summary>
         public string CellPhoneNumber { get; set; }
     }
 }

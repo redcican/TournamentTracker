e7a8bae982878453a7da3a4d8bc2daf4c3f81962
redcican <cican17@gmail.com>
2019-03-07 14:00:40 +0100

    Implement the InitializeConnections method.

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -8,17 +8,21 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
 
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if (database)
             {
-                // TODO - create the SQL connections
+                // TODO - Set up the SQL Connector properly.
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
             }
             if (textFiles)
             {
-                // TODO - create the Text connections
+                // TODO - Set up the Text connector properly.
+                TextConnection text = new TextConnection();
+                Connections.Add(text);
             }
         }
     }

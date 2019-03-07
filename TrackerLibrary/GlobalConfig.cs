27524ec7c79712d810a413bc448c5b9458e129d4
redcican <cican17@gmail.com>
2019-03-07 13:01:54 +0100

    Create a static class for global configuration.

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textfiles)
+        {
+            if (database)
+            {
+                // TODO - create the SQL connections
+            }
+            if (textfiles)
+            {
+                // TODO - create the Text connections
+            }
+        }
+    }
+}

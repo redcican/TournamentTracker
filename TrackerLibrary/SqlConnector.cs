fead7d5f0f8eb6a7e5a050cd9e8f2b7d6868fbbb
redcican <cican17@gmail.com>
2019-03-07 13:20:53 +0100

    Add TODO to the SqlConnector class and implement the CreatePrize method.

--- TrackerLibrary/SqlConnector.cs
+++ TrackerLibrary/SqlConnector.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 namespace TrackerLibrary
 {
     public class SqlConnector : IDataConnection
-    {
+    {   
+        // TODO - Make the CreatePrize method acutally save to the databse.
+        /// <summary>
+        /// Saves a new prize to the database       
+        /// </summary>
+        /// <param name="model">The prize information</param>
+        /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            
+            model.Id = 1;
+
+            return model;
         }
     }
 }

0bdfb7cd3317c367246ec18709bef80f3951a962
redcican <cican17@gmail.com>
2019-03-08 16:49:13 +0100

    config the txt file saving folders and add a class to help connect the txt file.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -12,9 +12,12 @@ namespace TrackerLibrary.DataAccess
         // TODO - Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-
-            return model;
+            // 1. Load the text file
+            // 2. Convert the text to List<Prizemodel>
+            // 3. Find the max ID
+            // 4. Add the new record to the new ID (max+1)
+            // 5. Convert the prizes to List<string>
+            // 6. Save the List<string> to the text file
         }
     }
 }
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.DataAccess.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        // fileName: e.g PrizeModels.csv
+        public static string FullFilePath(string fileName)
+        {
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{fileName}";
+        }
+    }
+}
